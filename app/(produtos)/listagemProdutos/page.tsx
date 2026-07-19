d6de28f721d25b7b60ea6f03c1d99b01dea452e1
Andre Alves <andre.ats050403@gmail.com>
2026-07-18 23:53:01 -0300

    ajuste suspense

--- app/(produtos)/listagemProdutos/page.tsx
+++ app/(produtos)/listagemProdutos/page.tsx
@@ -1,17 +1,22 @@
-import { CardDescription, CardTitle } from "@/components/ui/card";
-import { Fragment } from "react/jsx-runtime";
-import { ProdutosListagem } from "./component/ProdutosListagem";
-import { FiltroListagemProdutos } from "./component/FiltroListagemProdutos";
+import { Suspense } from "react";
 import { ListaGeral } from "./component/ListaGeral";
 
 export default function ListagemProdutos() {
     return (
-        <Fragment>
-            <div className="bg-fundoPrimaria w-full flex justify-center">
-                <div className="w-3/4 flex flex-row">
-                    <ListaGeral/>
-                </div>
+        <div className="flex w-full justify-center bg-fundoPrimaria">
+            <div className="flex w-3/4 flex-row">
+                <Suspense
+                    fallback={
+                        <div className="flex min-h-125 w-full items-center justify-center">
+                            <p className="text-white">
+                                Carregando produtos...
+                            </p>
+                        </div>
+                    }
+                >
+                    <ListaGeral />
+                </Suspense>
             </div>
-        </Fragment>
-    )
+        </div>
+    );
 }
