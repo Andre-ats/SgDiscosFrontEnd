7df7839bfefd7196ac9a01f8c57f6f31109d7442
Andre Alves <andre.ats050403@gmail.com>
2026-07-23 00:02:21 -0300

    suspense email page

--- app/(produtos)/email/page.tsx
+++ app/(produtos)/email/page.tsx
@@ -1,32 +1,37 @@
 'use client'
 
-import { Fragment } from "react/jsx-runtime";
+import { Suspense } from "react";
 import { CardEnviarEmail } from "./component/CardEnviarEmail";
 import { Button } from "@/components/ui/button";
 import { ArrowLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
 
 export default function EnviarEmail() {
-
     const router = useRouter();
 
     return (
-        <Fragment>
-            <div className="w-full bg-fundoPrimaria flex flex-col items-center">
-                <div className="w-3/4">
-                    <Button
-                        onClick={() => router.back()}
-                        className="cursor-pointer bg-primaria text-black hover:bg-[#ffcf0d] flex items-center gap-2 w-fit"
-                    >
-                        <ArrowLeft size={18} />
-                        Voltar
-                    </Button>
-                </div>
-
-                <div className="w-3/4 mt-6">
-                    <CardEnviarEmail />
-                </div>
+        <div className="flex w-full flex-col items-center bg-fundoPrimaria">
+            <div className="w-3/4">
+                <Button
+                    onClick={() => router.back()}
+                    className="flex w-fit cursor-pointer items-center gap-2 bg-primaria text-black hover:bg-[#ffcf0d]"
+                >
+                    <ArrowLeft size={18} />
+                    Voltar
+                </Button>
             </div>
-        </Fragment>
-    )
+
+            <div className="mt-6 w-3/4">
+                <Suspense
+                    fallback={
+                        <div className="text-white">
+                            Carregando formulário...
+                        </div>
+                    }
+                >
+                    <CardEnviarEmail />
+                </Suspense>
+            </div>
+        </div>
+    );
 }
